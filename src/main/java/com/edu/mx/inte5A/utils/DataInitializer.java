f0ef750a5616f3a8d032581f573a03cb87eea91a
emyRouge <20223tn028@utez.edu.mx>
2025-03-11 12:48:31 -0600

    datos de prueba

--- src/main/java/com/edu/mx/inte5A/utils/DataInitializer.java
+++ src/main/java/com/edu/mx/inte5A/utils/DataInitializer.java
@@ -1,7 +1,19 @@
 package com.edu.mx.inte5A.utils;
-
+import java.util.Date;
+import com.edu.mx.inte5A.Bien.Model.Bien;
+import com.edu.mx.inte5A.Bien.Model.BienRepository;
+import com.edu.mx.inte5A.Lugar.Model.Lugar;
+import com.edu.mx.inte5A.Lugar.Model.LugarRepository;
+import com.edu.mx.inte5A.Marca.Model.Marca;
+import com.edu.mx.inte5A.Marca.Model.MarcaRepository;
+import com.edu.mx.inte5A.Modelo.Model.Modelo;
+import com.edu.mx.inte5A.Modelo.Model.ModeloRepository;
 import com.edu.mx.inte5A.Rol.Model.Rol;
 import com.edu.mx.inte5A.Rol.Model.RolRepository;
+import com.edu.mx.inte5A.TipoBien.Model.TipoBien;
+import com.edu.mx.inte5A.TipoBien.Model.TipoBienRepository;
+import com.edu.mx.inte5A.Usuario.Model.Usuario;
+import com.edu.mx.inte5A.Usuario.Model.UsuarioRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +25,7 @@ import java.util.Optional;
 public class DataInitializer {
 
     @Bean
-    CommandLineRunner initDatabase( RolRepository rolRepository) {
+    CommandLineRunner initDatabase(RolRepository rolRepository, MarcaRepository marcaRepository, LugarRepository lugarRepository, ModeloRepository modeloRepository, TipoBienRepository tipoBienRepository, UsuarioRepository usuarioRepository, BienRepository bienRepository) {
         return args -> {
 
 
@@ -35,7 +47,35 @@ public class DataInitializer {
                 rolRepository.saveAndFlush(rol3);
 
             }
+            Lugar lugar1 = new Lugar( "Edificio Central", true, null);
+            Lugar lugar2 = new Lugar( "Laboratorio de Ciencias", true, null);
+            lugarRepository.saveAndFlush(lugar1);
+            lugarRepository.saveAndFlush(lugar2);
 
+
+
+            Marca marca1 = new Marca( "HP", true, null);
+            Marca marca2 = new Marca( "Dell", true, null);
+            Marca marca3 = new Marca( "Lenovo", true, null);
+
+            marcaRepository.saveAndFlush(marca1);
+            marcaRepository.saveAndFlush(marca2);
+            marcaRepository.saveAndFlush(marca3);
+
+            Modelo modelo1 = new Modelo("ss12", true, null, null);
+            Modelo modelo2 = new Modelo("sshhi3", true, null, null);
+            Modelo modelo3 = new Modelo("as3322", true, null, null);
+
+            modeloRepository.saveAndFlush(modelo1);
+            modeloRepository.saveAndFlush(modelo2);
+            modeloRepository.saveAndFlush(modelo3);
+
+            TipoBien tipoBien1 = new TipoBien("laptop",  true,null);
+            TipoBien tipoBien2 = new TipoBien("Pc",  true,null);
+            TipoBien tipoBien3 = new TipoBien("Tablet",  true,null);
+            tipoBienRepository.saveAndFlush(tipoBien1);
+            tipoBienRepository.saveAndFlush(tipoBien2);
+            tipoBienRepository.saveAndFlush(tipoBien3);
         };
     }
 }
